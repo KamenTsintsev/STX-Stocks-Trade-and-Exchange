51c2c7a2bb2dbbf784e2de5cefc27ecd69c07e35
unknown <shadow97791@gmail.com>
2022-06-29 16:10:03 +0300

    Add name to page

---
+++ src/components/views/Catalog/Catalog.js
@@ -0,0 +1,10 @@
+import "./Catalog.scss"
+
+
+export default function Catalog() {
+
+
+    return (
+        <h1>Catalog Page</h1>
+    )
+}
---
+++ src/components/views/Details/Details.js
@@ -0,0 +1,10 @@
+import "./Details.scss"
+
+
+export default function Details() {
+
+
+    return (
+        <h1>Details Page</h1>
+    )
+}
