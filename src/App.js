a6f49b0a116cb7c2b449c6fe7ebdf83f47c8c7a4
unknown <shadow97791@gmail.com>
2022-06-29 18:17:41 +0300

    add React-router and link the empty pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { } from "./App.scss"
+import "./App.scss"
+import { Routes, Route } from 'react-router-dom'
 import Header from "./components/Header/Header";
+import Catalog from './components/views/Catalog/Catalog'
 import CreateForm from './components/views/Create/Create'
+import Details from './components/views/Details/Details'
 
 function App() {
 	return (
 		<>
 			<Header></Header>
-			<CreateForm></CreateForm>
+
+			<div id="main-content">
+				<Routes>
+					<Route path="/" element={<Catalog />} />
+					<Route path="/create" element={<CreateForm />} />
+					<Route path="/details" element={<Details />} />
+				</Routes>
+			</div>
 		</>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./reset.scss"
@@ -8,7 +9,9 @@ import "./index.scss"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
