870cb0e5bc001fb594cff9708bac2a536ee4bead
unknown <shadow97791@gmail.com>
2022-06-29 15:58:46 +0300

    update user icon

--- src/components/Header/AccountBtn/GuestBtn.js
+++ src/components/Header/AccountBtn/GuestBtn.js
@@ -1,11 +1,19 @@
 import "./AccountBtn.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+// import userIcon from '../../Images/userIcon/userIcon-white.png'
+
 
 export default function GuestBtn() {
 
     return (
         <li className="user-btn guest">
             <p>Sign up</p>
-            <i className="fa-regular fa-circle-user"></i>
+            <div>
+                <FontAwesomeIcon icon={faUser} className={"i"}></FontAwesomeIcon>
+            </div>
+            {/* <img src={userIcon} alt="userIcon" className="userIcon" /> */}
+            {/* <i className="fa-regular fa-circle-user"></i> */}
         </li>
     )
 }
--- src/components/Header/AccountBtn/UserBtn.js
+++ src/components/Header/AccountBtn/UserBtn.js
@@ -1,11 +1,14 @@
 import "./AccountBtn.scss"
+import userIcon from '../../Images/userIcon/userIcon-white.png'
+
 
 export default function UserBtn() {
 
     return (
         <li className="user-btn user">
             <p>Sign up</p>
-            <i className="fa-regular fa-circle-user"></i>
+            <img src={userIcon} alt="userIcon" className="userIcon" />
+            {/* <i className="fa-regular fa-circle-user"></i> */}
         </li>
     )
 }
