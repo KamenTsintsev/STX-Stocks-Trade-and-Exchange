63097719542d61709df7002ef78ccdc79ce8c7c0
unknown <shadow97791@gmail.com>
2022-07-06 14:27:17 +0300

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
-import "./App.scss"
-import { Routes, Route } from 'react-router-dom'
+import "./App.scss";
+import { Routes, Route, Link, useLocation, Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Catalog from './components/views/Catalog/Catalog'
-import CreateForm from './components/views/Create/Create'
-import Details from './components/views/Details/Details'
+import Catalog from "./components/views/Catalog/Catalog";
+import CreateForm from "./components/views/Create/Create";
+import Details from "./components/views/Details/Details";
+import Authentication from "./components/views/Authentication/Authentication";
+import FormLogin from "./components/views/Authentication/FormLogin/FormLogin";
+import FormRegister from "./components/views/Authentication/FormRegister/FormRegister";
 
 function App() {
-	return (
-		<>
-			<Header></Header>
-		</>
-	);
+    const Layout = ({ hideHeaderPaths = "" }) => {
+        const { pathname } = useLocation();
+
+        return (
+            <>
+                {!pathname.includes(hideHeaderPaths) && <Header />}
+                <Outlet />
+            </>
+        );
+    };
+
+    return (
+        <>
+            <Header></Header>
+
+            <div id="mainContent">
+                <Routes>
+                    {/* <Route
+                        element={<Layout hideHeaderPaths={"/authentication"} />}
+                    > */}
+                    <Route path={"/"} exact element={<Catalog />}></Route>
+                    <Route
+                        path={"/create"}
+                        exact
+                        element={<CreateForm />}
+                    ></Route>
+                    <Route
+                        path="/authentication/*"
+                        element={<Authentication />}
+                    >
+                        <Route index element={<FormLogin />}></Route>
+                        <Route path="login" element={<FormLogin />}></Route>
+                        <Route
+                            path="register"
+                            element={<FormRegister />}
+                        ></Route>
+                    </Route>
+                    <Route path={"/details"} element={<Details />}></Route>
+                    {/* </Route> */}
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
