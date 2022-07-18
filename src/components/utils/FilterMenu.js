03605d3cafff1b9782483e419ac9c876aa62cfe1
unknown <shadow97791@gmail.com>
2022-07-18 19:49:34 +0300

    Add filter menu component

---
+++ src/components/utils/FilterMenu.js
@@ -0,0 +1,153 @@
+import "./FilterMenu.scss";
+
+export default function FilterMenu() {
+    return (
+        <aside className="filterMenuContainer">
+            <h4>Filters</h4>
+            <form>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+
+                <div className="filterFragment">
+                    <p className="bold text">Name</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Price</p>
+                    <label className="text-14px" htmlFor="min-price">
+                        MIN
+                    </label>
+                    <input
+                        type="number"
+                        name="name"
+                        id="min-price"
+                        className="priceRange"
+                    />
+                    {" - "}
+                    <label className="text-14px" htmlFor="max-price">
+                        MAX
+                    </label>
+                    <input
+                        type="number"
+                        name="name"
+                        id="max-price"
+                        className="priceRange"
+                    />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+                <div className="filterFragment">
+                    <p className="bold text">Quality</p>
+                    <label className="text-14px" htmlFor="name">
+                        Name
+                    </label>
+                    <input type="checkbox" name="name" id="name" />
+                </div>
+            </form>
+        </aside>
+    );
+}
