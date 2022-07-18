0829db9a38f6136da7854ba3d1cdff16066bc129
unknown <shadow97791@gmail.com>
2022-07-18 20:06:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import "./reset.scss"
-import "./index.scss"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./reset.scss";
+import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
-      <App />
+        <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
