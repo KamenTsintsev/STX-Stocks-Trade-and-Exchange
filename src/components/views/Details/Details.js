09c10bfc9fb306bd9f096049b4121584e03b14fc
unknown <shadow97791@gmail.com>
2022-07-18 21:06:13 +0300

    Update Details.js

--- src/components/views/Details/Details.js
+++ src/components/views/Details/Details.js
@@ -12,7 +12,7 @@ export default function Details() {
             "Color night vision: An all-new Starlight Sensor records night time video in full, vivid color. The Starlight Sensor can see full color in environments up to 25x darker than traditional video cameras and the new f/1.6 aperture captures 2x more light. Indoor/ Outdoor: Wyze Cam v3 is a wired video camera with an IP65 rating so you can confidently install it outside in the rain or inside in the kids' room.Wyze Outdoor Power Adapter(sold separately) required for outdoor use.Phone Compatibility - Android 5.0 +, iOS 9.0 +. Motion & Sound detection: Wyze Cam records video when motion or sound is detected and sends an alert straight to your phone.Motion Detection Zones and custom settings allow you to adjust the sensitivity of detection or turn it off completely. 24 / 7 Continuous Recording: Continuous video recording with a 32GB MicroSD card(sold separately).Just insert the MicroSD into the base of the Wyze Cam and you're all set. IFTTT certified connect all of your different apps and devices.When you sign up for a free account, you can enable your apps and devices to work together.",
         price: 35.98,
         imageArr: [
-            require("../../Images/products/chopper/4.jpg"),
+            require("../../../Images/products/chopper/4.jpg"),
             "https://i.pinimg.com/736x/69/82/7f/69827f4b51b0d827e82cb785808f1d87--amazing-cars-muscle.jpg",
             "http://www.stancenation.com/wp-content/uploads/2014/08/Stanced-Bagged-Nissan-350Z-1.jpg",
             "http://www.stancenation.com/wp-content/uploads/2014/08/Stanced-Bagged-Nissan-350Z-4.jpg",
