c7e5c51490818cc63f0daaf4629bc5b9ea5a27b5
Kamen Tsintsev <shadow97791@gmail.com>
2022-08-08 16:43:15 +0300

    Simplify Logout.js
    Changed to only use the AuthContext

--- src/components/utils/Logout.js
+++ src/components/utils/Logout.js
@@ -1,20 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import * as userAPI from "../../api/UserAPI";
+import { useContext, useEffect } from "react";
 import AuthContext from "../../contexts/authenticationContext";
 
 const Logout = () => {
     const userContext = useContext(AuthContext);
-    const navigate = useNavigate();
 
     useEffect(() => {
-        try {
-            userContext.logout();
-            navigate("/");
-        } catch (err) {
-            navigate("/");
-            throw err;
-        }
+        userContext.logout();
     }, []);
 };
 
