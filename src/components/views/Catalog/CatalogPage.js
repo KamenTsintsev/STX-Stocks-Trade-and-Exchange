9e5e34513070bea459e200a6b4fd2958c0b3671a
Kamen Tsintsev <shadow97791@gmail.com>
2022-08-08 16:51:25 +0300

    Adapt CatalogPage to show myItems

--- src/components/views/Catalog/CatalogPage.js
+++ src/components/views/Catalog/CatalogPage.js
@@ -1,18 +1,25 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import FilterMenu from "../../utils/FilterMenu";
 import ProductsToolbar from "../../utils/ProductsToolbar";
 import ItemsArea from "./ItemsArea";
 import * as dataApi from "../../../api/DataAPI";
+import AuthContext from "../../../contexts/authenticationContext";
 
 import "./CatalogPage.scss";
 
-export default function Catalog() {
+export default function Catalog({ isMyAccount }) {
     const [items, setItems] = useState([]);
+    const { userId } = useContext(AuthContext);
 
     useEffect(() => {
         const getItems = async () => {
-            const itemsRes = await dataApi.getAllItems();
-            setItems(itemsRes);
+            if (isMyAccount) {
+                const itemsRes = await dataApi.getMyItems(userId);
+                setItems(itemsRes);
+            } else {
+                const itemsRes = await dataApi.getAllItems();
+                setItems(itemsRes);
+            }
         };
 
         getItems();
